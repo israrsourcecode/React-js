13c114768150f245c49f60cad31a9246b375bfdb
israrsourcecode <israr.sourcecode@gmail.com>
2026-03-26 21:33:44 +0500

    "React-Router-pracetice"

---
+++ react-router/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Navbar from './components/Navbar'
+import Contact from './pages/contact'
+
+const App = () => {
+  return (<>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+  </>
+  )
+}
+
+export default App
